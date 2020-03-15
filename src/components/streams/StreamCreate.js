5e14727e331133acfe1a335884ca4d7e9a6aeec4
Julius <julius@pop-os.localdomain>
2020-03-15 19:08:41 +0100

    Add redux-forms and make new streams view
    Because:
    - redux forms abstracts away a lot of tedious work
    - new strams page was not created

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,61 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderInput = ({ input, label, meta }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+        <div>{this.renderError(meta)}</div>
+      </div>
+    );
+  };
+
+  renderError({ touched, error }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">dingo</div>
+        </div>
+      );
+    }
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter title:" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Please enter a title.";
+  }
+  if (!formValues.description) {
+    errors.description = "Please enter a meaningful description.";
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  validate
+})(StreamCreate);
