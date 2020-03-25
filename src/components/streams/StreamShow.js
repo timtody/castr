20cd3eeff081e80c7771cd20f2bf1d2204efaa9b
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-03-25 18:15:10 +0100

    Complete CRUD base

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { getStream } from "../../actions";
+
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.getStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading</div>;
+    }
+    const { title, description } = this.props.stream;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { getStream })(StreamShow);
